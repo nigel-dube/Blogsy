fe247c1fb525de99a42439c63ddce4601ee0c37e
nigel-dube <nigel.dbe@gmail.com>
2025-08-05 15:28:03 -0600

    fix chaining in dashboard page

--- app/dashboard/page.tsx
+++ app/dashboard/page.tsx
@@ -1,44 +1,47 @@
-import { buttonVariants } from "@/components/ui/button";
 import Link from "next/link";
-import { prisma } from "../utils/db";
+import { buttonVariants } from "@/components/ui/button";
+import { prisma } from "@/app/utils/db";
 import { getKindeServerSession } from "@kinde-oss/kinde-auth-nextjs/server";
 import { BlogPostCard } from "@/components/general/BlogPostCard";
 
 async function getData(userId: string) {
-  await new Promise((resolve) => setTimeout(resolve, 2000));
-  const data = await prisma.blogPost.findMany({
-    where: {
-      authorId: userId,
-    },
-    orderBy: {
-      createdAt: "desc",
-    },
-  });
+    await new Promise((resolve) => setTimeout(resolve, 2000));
+    const data = await prisma.blogPost.findMany({
+        where: {
+            authorId: userId
+        },
+        orderBy: {
+            createdAt: "desc"
+        },
+    
+    })
 
-  return data;
+    return data;
 }
 
-export default async function DashboardRoute() {
-  const { getUser } = getKindeServerSession();
-  const user = await getUser();
+export default async function DashboardRoute () {
+    const { getUser } = getKindeServerSession();
+    const user = await getUser();
+    if (!user) {
+        return <div className="text-center">You need to be logged in to view this page.</div>;
+    }
+    const data = await getData(user.id);
 
-  const data = await getData(user?.id);
+    return (
+        <div>
+            <div className="flex items-center justify-between mb-4">
+                <h2 className="text-xl font-medium">Your Blog Articles</h2>
 
-  return (
-    <div>
-      <div className="flex items-center justify-between mb-4">
-        <h2 className="text-xl font-medium">Your Blog Articles</h2>
+                <Link href="/dashboard/create" className={buttonVariants()}>
+                    Create Post
+                </Link>
+            </div>
 
-        <Link className={buttonVariants()} href="/dashboard/create">
-          Create Post
-        </Link>
+            <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-4">
+              {data.map((item) => (
+                <BlogPostCard data={item} key={item.id} />
+             ))}
       </div>
-
-      <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-4">
-        {data.map((item) => (
-            <BlogPostCard data={item} key={item.id} />
-        ))}
-      </div>
-    </div>
-  );
+        </div>
+    )
 }
